49dc282fbb3c0b3804c3b2c931832ebf410c3dd4
Julian-Alberts <julian.alberts@ewe.net>
2024-05-25 17:36:13 +0200

    Refactored iterators to be "generic" over there return type

--- src/iter.rs
+++ src/iter.rs
@@ -45,34 +45,47 @@ where
 {
     type Item = &'a (Point<PU>, Item);
     fn next(&mut self) -> Option<Self::Item> {
-        'main: loop {
-            let ctx = self.stack.last_mut()?;
-            if let Some(quads) = &mut ctx.quadrants {
-                while !quads.is_empty() {
-                    let quad = &quads[0];
-                    *quads = &quads[1..];
-                    if ctx.is_enclosed_by_area || self.area.intersects(&quad.boundary) {
-                        let int_query = InternQuery::new(quad, ctx.is_enclosed_by_area, &self.area);
-                        self.stack.push(int_query);
-                        continue 'main;
-                    }
-                }
-                ctx.quadrants = None
-            }
+        query_next(&mut self.stack, &self.area)
+    }
+}
 
-            if let Some(items) = &mut ctx.items {
-                while !items.is_empty() {
-                    let item = &items[0];
-                    *items = &items[1..];
-                    if ctx.is_enclosed_by_area || self.area.contains(&item.0) {
-                        return Some(item);
-                    }
+fn query_next<'a, TreeItem, C, A, Cap, RetItem>(
+    stack: &mut Vec<InternQuery<'a, C, TreeItem, Cap>>,
+    area: &A,
+) -> Option<&'a RetItem>
+where
+    RetItem: FromTreeItem<TreeItem, C>,
+    C: Coordinate,
+    A: Area<C>,
+    Cap: Capacity,
+{
+    'main: loop {
+        let ctx = stack.last_mut()?;
+        if let Some(quads) = &mut ctx.quadrants {
+            while !quads.is_empty() {
+                let quad = &quads[0];
+                *quads = &quads[1..];
+                if ctx.is_enclosed_by_area || area.intersects(&quad.boundary) {
+                    let int_query = InternQuery::new(quad, ctx.is_enclosed_by_area, area);
+                    stack.push(int_query);
+                    continue 'main;
                 }
-                ctx.quadrants = None;
             }
-
-            self.stack.pop();
+            ctx.quadrants = None
         }
+
+        if let Some(items) = &mut ctx.items {
+            while !items.is_empty() {
+                let item = &items[0];
+                *items = &items[1..];
+                if ctx.is_enclosed_by_area || area.contains(&item.0) {
+                    return Some(RetItem::from_iter_type(item));
+                }
+            }
+            ctx.quadrants = None;
+        }
+
+        stack.pop();
     }
 }
 
@@ -102,9 +115,15 @@ where
     Cap: Capacity,
     PU: Coordinate,
 {
-    quadrants: Option<&'a [QuadTree<PU, Item, Cap>]>,
-    items: Option<&'a [(Point<PU>, Item)]>,
-    current_sub_query: Option<Box<IterPoints<'a, PU, Item, Cap>>>,
+    stack: Vec<IterIntern<'a, PU, Item, Cap>>
+}
+
+#[derive(Clone)]
+struct IterIntern<'a, C, Item, Cap>
+    where C: Coordinate
+{
+    quadrants: Option<&'a [QuadTree<C, Item, Cap>]>,
+    items: Option<&'a [(Point<C>, Item)]>,
 }
 
 impl<'a, PU, Item, Cap> IterPoints<'a, PU, Item, Cap>
@@ -113,21 +132,7 @@ where
     PU: Coordinate,
 {
     pub(super) fn new(tree: &'a QuadTree<PU, Item, Cap>) -> Self {
-        Self {
-            items: tree.items.as_deref(),
-            quadrants: tree.quadrants.as_ref().map(|q| q.as_slice()),
-            current_sub_query: None,
-        }
-    }
-
-    fn find_next_quadrant(&mut self) -> Option<Box<IterPoints<'a, PU, Item, Cap>>> {
-        let quadrants = self.quadrants.as_mut()?;
-        if quadrants.is_empty() {
-            return None;
-        }
-        let q = &quadrants[0];
-        *quadrants = &quadrants[1..];
-        return Some(Box::new(q.iter_points()));
+        Self { stack: vec![IterIntern {quadrants: tree.quadrants.as_ref().map(|q| q.as_slice()), items: tree.items.as_ref().map(|items| items.as_slice())}] }
     }
 }
 
@@ -139,25 +144,38 @@ where
     type Item = &'a (Point<PU>, Item);
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.items.map(|i| !i.is_empty()).unwrap_or_default() {
-            let item = &self.items.unwrap()[0];
-            self.items = self.items.map(|i| &i[1..]);
-            return Some(item);
-        }
-        if self.current_sub_query.is_none() {
-            self.current_sub_query = self.find_next_quadrant();
+        iter_next(&mut self.stack)
+    }
+}
+
+fn iter_next<'a, C, TreeItem, RetItem, Cap>(
+    stack: &mut Vec<IterIntern<'a, C, TreeItem, Cap>>
+) -> Option<&'a RetItem>
+    where C: Coordinate, RetItem: FromTreeItem<TreeItem, C>, Cap: Capacity
+{
+    loop {
+        let ctx = stack.last_mut()?;
+        if let Some(items) = &mut ctx.items {
+            if !items.is_empty() {
+                let item = &items[0];
+                *items = &items[1..];
+                return Some(RetItem::from_iter_type(item))
+            }
+            ctx.items = None;
         }
 
-        let Some(current_query) = self.current_sub_query.as_mut() else {
-            return None;
-        };
-
-        if let Some(item) = current_query.next() {
-            return Some(item);
+        if let Some(quadrants) = &mut ctx.quadrants {
+            if !quadrants.is_empty() {
+                let quad = &quadrants[0];
+                *quadrants = &quadrants[1..];
+                stack.push(IterIntern { quadrants: quad.quadrants.as_ref().map(|q| q.as_slice()), items: quad.items.as_ref().map(Vec::as_slice) });
+            } else {
+                ctx.quadrants = None;
+                stack.pop();
+            }
+        } else {
+            stack.pop();
         }
-
-        self.current_sub_query = None;
-        self.next()
     }
 }
 
@@ -189,7 +207,7 @@ where
 {
     type Item = &'a Item;
     fn next(&mut self) -> Option<Self::Item> {
-        self.0.next().map(|pos| &pos.1)
+        query_next(&mut self.0.stack, &self.0.area)
     }
 }
 
@@ -219,6 +237,33 @@ where
     type Item = &'a Item;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.0.next().map(|pos| &pos.1)
+        iter_next(&mut self.0.stack)
+    }
+}
+
+trait FromTreeItem<Item, C>
+where
+    C: Coordinate,
+{
+    fn from_iter_type(t: &(Point<C>, Item)) -> &Self;
+}
+
+impl<Item, C> FromTreeItem<Item, C> for (Point<C>, Item)
+where
+    C: Coordinate,
+{
+    #[inline]
+    fn from_iter_type(t: &(Point<C>, Item)) -> &Self {
+        &t
+    }
+}
+
+impl<Item, C> FromTreeItem<Self, C> for Item
+where
+    C: Coordinate,
+{
+    #[inline]
+    fn from_iter_type(t: &(Point<C>, Self)) -> &Self {
+        &t.1
     }
 }
